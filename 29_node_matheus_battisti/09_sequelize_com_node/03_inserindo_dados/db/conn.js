716a33425cebd777bebd03d21811ab2e6134f3a1
Perkles <albery.reis.88@gmail.com>
2024-08-14 20:43:02 -0300

    curso de sequelize com matheus batisti finalizado

---
+++ 29_node_matheus_battisti/09_sequelize_com_node/03_inserindo_dados/db/conn.js
@@ -0,0 +1,28 @@
+// const mysql = require('mysql')
+
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: 'localhost',
+//   user: 'admin',
+//   password: '23050904',
+//   database: 'nodemysql2'
+// })
+
+// module.exports = pool
+
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize2', 'admin', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  password: '23050904',
+})
+
+// try {
+//   sequelize.authenticate()
+//   console.log('Conectamos com sucesso com o sequilize!')
+// } catch (error) {
+//   console.log('Não foi possivel conectar: ', error)
+// }
+
+module.exports = sequelize
---
+++ 29_node_matheus_battisti/09_sequelize_com_node/10_add_relacionado/models/Address.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn.js')
+
+const User = require('./User.js')
+
+const Address = db.define('Address', {
+  street: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  number: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+})
+
+Address.belongsTo(User)
+
+module.exports = Address
