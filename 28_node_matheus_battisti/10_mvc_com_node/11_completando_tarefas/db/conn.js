91f102ba1497813e920534a8257832740d488e17
Perkles <albery.reis.88@gmail.com>
2024-08-27 12:54:37 -0300

    curso de mvc em node com matheus batisti finalizado

---
+++ 28_node_matheus_battisti/10_mvc_com_node/11_completando_tarefas/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc2', 'admin', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  password: '23050904'
+})
+
+try {
+  sequelize.authenticate()
+  console.log('Conectamos ao MySQL...')
+} catch (error) {
+  console.log(`Não foi possivel conectar: ${error}`)
+}
+
+module.exports = sequelize
